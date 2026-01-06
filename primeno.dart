49fe4255a2379af2d21153a45c62477f04ff8daa
Arif khan <arif0852147@gmail.com>
2026-01-06 17:00:25 +0500

    prime number

---
+++ primeno.dart
@@ -0,0 +1,17 @@
+void main() {
+  int number = 29;
+  bool isPrime = true;
+
+  if (number <= 1) {
+    isPrime = false;
+  } else {
+    for (int i = 2; i <= number ~/ 2; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+  }
+
+  print(isPrime ? "$number is Prime" : "$number is Not Prime");
+}
