ce972e4e85cad7f932a95edb574daf4e3f1e5452
Arif khan <arif0852147@gmail.com>
2026-01-09 15:58:44 +0500

    perfect_number

---
+++ perfect_number.dart
@@ -0,0 +1,16 @@
+void main() {
+  int number = 28;
+  int sum = 0;
+
+  for (int i = 1; i <= number ~/ 2; i++) {
+    if (number % i == 0) {
+      sum += i;
+    }
+  }
+
+  print(
+    sum == number
+        ? "$number is a Perfect Number"
+        : "$number is not a Perfect Number",
+  );
+}
