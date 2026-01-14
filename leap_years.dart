d6a8fdc73b338b32546d7b32ebdc5fb417820c39
Arif khan <arif0852147@gmail.com>
2026-01-14 17:50:56 +0500

    leap_years

---
+++ leap_years.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  int year = int.parse(stdin.readLineSync()!);
+
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    print("Leap Year");
+  } else {
+    print("Not a Leap Year");
+  }
+}
