9b34c8b3bacb44fcf279bf732e3b3c68b7f01eef
Arif khan <arif0852147@gmail.com>
2026-01-16 14:20:42 +0500

    perfect

---
+++ perfect.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int n = int.parse(stdin.readLineSync()!);
+  int sum = 0;
+
+  for (int i = 1; i <= n ~/ 2; i++) {
+    if (n % i == 0) sum += i;
+  }
+
+  print(sum == n ? "Perfect Number" : "Not Perfect");
+}
